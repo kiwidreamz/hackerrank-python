7dfaace6525762cb67f46d1014768c391965d7ea
Stephane Popov <73848169+kiwidreamz@users.noreply.github.com>
2022-05-22 16:58:57 +0200

    mutations

---
+++ mutations.py
@@ -0,0 +1,8 @@
+# Read a given string, change the character at a given index and then print the modified string.
+
+def mutate_string(string, position, character):
+    
+    string = string[:position] + character + string[position+1:]
+    
+    return string
+
